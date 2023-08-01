396703111dc9bde0f5256255c88e0244f837f00d
Sebastián Araos <saraos.3it@bancochile.cl>
2023-08-01 14:44:02 -0400

    Init project

---
+++ src/main/java/cl/saraos/bank/ExamBankApplication.java
@@ -0,0 +1,13 @@
+package cl.saraos.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamBankApplication.class, args);
+	}
+
+}
